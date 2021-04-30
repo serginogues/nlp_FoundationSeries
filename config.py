2cf85fdfff8b145301783960c23a1a27e191cf48
Sergi Nogués Farrés <nogues.s.6@gmail.com>
2021-04-30 16:47:32 +0200

    comments

--- config.py
+++ config.py
@@ -15,6 +15,37 @@ import pandas as pd
 nlp = spacy.load("en_core_web_sm")
 matcher = Matcher(nlp.vocab)
 neuralcoref.add_to_pipe(nlp)
+# pip install -r requirements.txt
+
+"""
+- Preprocessing
+- Entity extraction with Ruled Based + Snorkel
+- Coreference and negation handling
+- Validation
+- Visualization
+https://arxiv.org/pdf/1907.02704.pdf
+1) The identification of characters. We distinguish two substeps:
+    - Detect occurrences of characters in the narrative
+        -  a character can appear under three forms in text: proper noun, nominal, and pronoun.
+    - Unify these occurrences, i.e. to determine which ones correspond to the same character.
+    In a text, the same character can appear under different names.
+    The output of this step takes the form of a chronological sequence of unified character occurrences.
+
+2) Detecting interactions between characters.
+    - take into account conversations, and to consider that two characters interact when one talks to the other.
+    With certain forms of narrative such as plays, in which speakers are given, this task is relatively straightforward.
+    - situations where one character talks about the other.
+    - all sorts of actions one character can perform on the other (besides conversing).
+    - explicit or inferred social relationships such as being married, being relatives, or working together
+    The output of the second step is a chronological sequence of interactions between characters.
+
+3) The extraction of the proper graph
+    - preprocess: simplifying the sequence of interactions by filtering and/or merging some of the characters under certain conditions.
+    For example, when considering co-occurrences, some authors merge characters that always appear together
+    - temporal integration, i.e. the aggregation of the previously identified interactions.
+        - full integration and therefore leading to a static network, and those performing only
+        a partial integration, and producing a dynamic network.
+"""
 
 """
 Related work
--- main.py
+++ main.py
@@ -1,27 +1,16 @@
 """
-pip install -r requirements.txt
-https://arxiv.org/pdf/1907.02704.pdf
-1) The identification of characters. We distinguish two substeps:
-    - Detect occurrences of characters in the narrative
-        -  a character can appear under three forms in text: proper noun, nominal, and pronoun.
-    - Unify these occurrences, i.e. to determine which ones correspond to the same character.
-    In a text, the same character can appear under different names.
-    The output of this step takes the form of a chronological sequence of unified character occurrences.
-
-2) Detecting interactions between characters.
-    - take into account conversations, and to consider that two characters interact when one talks to the other.
-    With certain forms of narrative such as plays, in which speakers are given, this task is relatively straightforward.
-    - situations where one character talks about the other.
-    - all sorts of actions one character can perform on the other (besides conversing).
-    - explicit or inferred social relationships such as being married, being relatives, or working together
-    The output of the second step is a chronological sequence of interactions between characters.
-
-3) The extraction of the proper graph
-    - preprocess: simplifying the sequence of interactions by filtering and/or merging some of the characters under certain conditions.
-    For example, when considering co-occurrences, some authors merge characters that always appear together
-    - temporal integration, i.e. the aggregation of the previously identified interactions.
-        - full integration and therefore leading to a static network, and those performing only
-        a partial integration, and producing a dynamic network.
+Report in research paper format
+Sections:
+    - Abstract
+    - Introduction
+    - Background (related work)
+    - Approach
+    - Dataset (book + Validation points)
+    - Algorithms
+    - Experiments
+    - Future research
+    - References
+Add as well in the report what doubts we have and what is not clear
 """
 
 from entity_identification import *
