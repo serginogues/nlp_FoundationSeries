6b470f0473fb577c9ec3db59ba0fdf336ea9cde5
Sergi Nogués Farrés <nogues.s.6@gmail.com>
2021-05-18 23:47:02 +0200

    get sentences

--- preprocess.py
+++ preprocess.py
@@ -1,25 +1,26 @@
-from config import sent_tokenize, re, tqdm, nlp, STAGE, FoundationTrilogy
+from config import sent_tokenize, re, tqdm, nlp, STAGE
 
 
-def preprocess():
+def preprocess(text):
     """
     Remove unwanted characters + split by sentences + sentence tokenization + parsing + lemmatization + POS tagging
     """
     print("Start PREPROCESS")
     doc_list = []
     if STAGE < 3:
-        text = remove_from_text(FoundationTrilogy)
-
-        # 1 - original sentence
-        sentences = sent_tokenize(text)
-        sentences = [re.sub(' +', ' ', sent) for sent in sentences]
-
-        # English tokenizer, tagger, parser and NER
-
+        sentences = get_sentences(text)
         for i in tqdm(range(len(sentences))):
             doc_list.append(nlp(sentences[i]))
+
         print("Number of preprocessed sentences: ", len(doc_list))
     return doc_list
+
+
+def get_sentences(text):
+    text = remove_from_text(text)
+    sentences = sent_tokenize(text)
+    sentences = [re.sub(' +', ' ', sent) for sent in sentences]
+    return sentences
 
 
 def remove_from_text(text):
@@ -39,5 +40,4 @@ def split_by_num_charcacters(text, n=500000):
     """
     https://stackoverflow.com/questions/9475241/split-string-every-nth-character
     """
-    return [text[i:i+n] for i in range(0, len(text), n)]
-
+    return [text[i:i + n] for i in range(0, len(text), n)]
